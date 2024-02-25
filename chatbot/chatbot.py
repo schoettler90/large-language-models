87e0984abccc6b399a0553cc12358e6820df18f7
schoettler90 <ricardo.schoettler@gmail.com>
2024-02-25 10:40:54 +0100

    update chatbot.py ++ gemma model

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
 
 # if cuda is available, use it
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# add your hugingface token to the environment variables
 ACCESS_TOKEN = os.getenv("HUGGINGFACE_WRITE")
 
-MODEL_PATH = r"D:\models\Llama-2-7b-chat-hf"
-# MODEL_PATH = r"D:\models\Orca-2-7b"
+# if the model is locally store, set an .env variable with the model name
+# MODEL_PATH = os.getenv("LLAMA", "meta-llama/Llama-2-7b-chat-hf")
+MODEL_PATH = os.getenv("GEMMA", "google/gemma-7b")
 
 
 def load_model():
@@ -39,6 +42,9 @@ def load_model():
 def chat_with_model(model, tokenizer):
     print("Welcome! You can start a conversation by typing your message, or type 'exit' to end the conversation.")
 
+    # initialize conversation
+    conversation = ""
+
     while True:
         user_input = input("You: ")
 
@@ -47,8 +53,11 @@ def chat_with_model(model, tokenizer):
             print("Goodbye!")
             break
 
+        # add user input to conversation
+        conversation += user_input + "\n"
+
         # Encode the user's input and generate a response
-        input_ids = tokenizer.encode(user_input, return_tensors="pt").to(DEVICE)
+        input_ids = tokenizer.encode(conversation, return_tensors="pt").to(DEVICE)
         output = model.generate(
             input_ids,
             max_length=1000,
@@ -58,8 +67,14 @@ def chat_with_model(model, tokenizer):
             pad_token_id=tokenizer.eos_token_id,
         )
 
-        # Decode and print the model's response
-        print("Model:", tokenizer.decode(output[0], skip_special_tokens=True))
+        # Decode the model's response
+        response = tokenizer.decode(output[0], skip_special_tokens=True)
+
+        # Print the model's response
+        print("Model:", response)
+
+        # add model response to conversation
+        conversation += response + "\n"
 
 
 def run_chatbot():
